2aaa256aa972f1e84d31848eed5a7f97c7989df1
taybauer <102003686+taybauer@users.noreply.github.com>
2022-08-06 14:52:17 -0500

    Added bootstrap and logo, built first draft of search engine in /Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <h1>Hello World</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Open-source code by Taylor Bauer. Hosted on Netlify.
+      </footer>
     </div>
   );
 }
