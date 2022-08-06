639c250d0328cba5075a02eec70ecdaf6b574da2
taybauer <102003686+taybauer@users.noreply.github.com>
2022-08-06 15:31:36 -0500

    bug fixes

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(resposne.data[0]);
+    console.log(response.data[0]);
   }
 
   function search(event) {
