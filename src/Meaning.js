4950e4a4eca397a674a81dff8275f1aff0743195
taybauer <102003686+taybauer@users.noreply.github.com>
2022-08-07 17:39:17 -0500

    Styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -11,10 +11,9 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition: </strong>
-              {definition.definition}
+              <div className="definition">{definition.definition}</div>
               <br />
-              <em>{definition.example}</em>
+              <div className="example">{definition.example}</div>
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div className="Synonyms">
+      <ul className="Synonyms">
         {props.synonyms.map(function (synonyms, index) {
-          return <span key={index}>{synonyms}</span>;
+          return <li key={index}>{synonyms}</li>;
         })}
-      </div>
+      </ul>
     );
   } else {
     return null;
